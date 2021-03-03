2b3af6d67fb61a6100f2e0f58afbc9c01ddb9942
lyhace <364725418@qq.com>
2021-03-03 18:32:10 +0800

    增加Eureka

---
+++ sfnotice/src/main/java/com/lyhace/sfnotice/SfnoticeApplication.java
@@ -0,0 +1,13 @@
+package com.lyhace.sfnotice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfnoticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfnoticeApplication.class, args);
+    }
+
+}
---
+++ sfnotice/src/test/java/com/lyhace/sfnotice/SfnoticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyhace.sfnotice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfnoticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
