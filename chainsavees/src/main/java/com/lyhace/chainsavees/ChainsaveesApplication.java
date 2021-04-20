560c2f9ca07ef67aaf4a2746d46751165b8e3727
lyhace <364725418@qq.com>
2021-04-20 18:06:31 +0800

    初始化rocketmq数据录入elasticsearch

---
+++ chainsavees/src/main/java/com/lyhace/chainsavees/ChainsaveesApplication.java
@@ -0,0 +1,13 @@
+package com.lyhace.chainsavees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainsaveesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChainsaveesApplication.class, args);
+    }
+
+}
---
+++ chainsavees/src/test/java/com/lyhace/chainsavees/ChainsaveesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyhace.chainsavees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChainsaveesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
